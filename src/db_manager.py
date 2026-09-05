ca45405901cb7324e335ee12b781e122415b7bcb
Nikodim26 <lr1975monk@gmail.com>
2026-09-05 20:07:01 +0400

    создал четыре пробные таблицы

--- src/db_manager.py
+++ src/db_manager.py
@@ -8,6 +8,19 @@ class DBManager():
         self.db_name = db_name
         self.path = Path(__file__).resolve().parent.parent / filename
 
+        params = self.config()
+        conn = psycopg2.connect(dbname='postgres', **params)
+        conn.autocommit = True
+        cur = conn.cursor()
+
+        cur.execute(f"SELECT 1 FROM pg_database WHERE datname = '{db_name}'")
+        ex=cur.fetchone()
+        if not ex:
+            cur.execute(f"CREATE DATABASE {db_name}")
+
+        cur.close()
+        conn.close()
+
     def config(self, section="postgresql") -> dict:
         """Считывает параметры подключения к postgresql"""
 
@@ -23,18 +36,6 @@ class DBManager():
                 'Section {0} is not found in the {1} file.'.format(section, self.path))
         return db
 
-    def creating_a_database(self) -> None:
-
-        params = self.config()
-        conn = psycopg2.connect(dbname='postgres', **params)
-        cur = conn.cursor()
-
-        cur.execute(f"SELECT 1 FROM pg_database WHERE datname = '{self.db_name}'")
-        if not cur.fetchone():
-            cur.execute(f"CREATE DATABASE {self.db_name}")
-
-        cur.close()
-        conn.close()
 
     def working_with_the_base(self, request: str) -> None:
         """Работает с базой данных"""
@@ -50,14 +51,14 @@ class DBManager():
         conn.close()
 
 
-dbm = DBManager('aaa', 'database.ini')
-dbm.creating_a_database()
-dbm.working_with_the_base('''CREATE TABLE IF NOT EXISTS channels (
-                            channel_id SERIAL PRIMARY KEY,
-                            title VARCHAR(255) NOT NULL,
-                            views INTEGER,
-                            subscribers INTEGER,
-                            videos INTEGER,
-                            channel_url TEXT
-                        )
-                        ''')
+# dbm = DBManager('aaa', 'database.ini')
+# dbm.creating_a_database()
+# dbm.working_with_the_base('''CREATE TABLE IF NOT EXISTS channels (
+#                             channel_id SERIAL PRIMARY KEY,
+#                             title VARCHAR(255) NOT NULL,
+#                             views INTEGER,
+#                             subscribers INTEGER,
+#                             videos INTEGER,
+#                             channel_url TEXT
+#                         )
+#                         ''')
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from src.api_airplanes import ApiAeroplanes
 from src.api_coord import ApiCoord
+from src.db_manager import DBManager
 
 
 def creation_of_aircraft_database(countries:list[str]) -> None:
@@ -9,8 +10,18 @@ def creation_of_aircraft_database(countries:list[str]) -> None:
     for country in countries:
         api_coord = ApiCoord(country)
         api_aeroplanes = ApiAeroplanes(api_coord.coordinates).list_info
-        
 
+        dbm = DBManager('airplanes', 'database.ini')
+
+        dbm.working_with_the_base(f'''CREATE TABLE IF NOT EXISTS tb_{country} (
+                                channel_id SERIAL PRIMARY KEY,
+                                title VARCHAR(255) NOT NULL,
+                                views INTEGER,
+                                subscribers INTEGER,
+                                videos INTEGER,
+                                channel_url TEXT
+                            )
+                            ''')
 
 
 
