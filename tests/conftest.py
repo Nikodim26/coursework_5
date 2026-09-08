4012a5bce4373954d52cab4925f782100fdd1538
Андрей Васильев <lr1975@yandex.ru>
2026-09-08 11:41:55 +0400

    пока все

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,34 +1,20 @@
+from unittest.mock import patch, MagicMock
+
 import pytest
 
 from src.db_manager import DBManager
-import psycopg2
-
 
 @pytest.fixture
-def fixture_for_object_construction() -> DBManager:
+@patch('psycopg2.connect')
+def fixture_for_object_construction(mock_connect):
+
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+
+    mock_connect.return_value = mock_conn
+    mock_conn.cursor.return_value = mock_cursor
+    mock_cursor.execute.return_value = None
+
     dbm = DBManager('db_name', 'database.ini', ['a'])
 
-    conn = psycopg2.connect(dbname='postgres', **dbm.params)
-    conn.autocommit = True
-    cur = conn.cursor()
-    cur.execute('DROP DATABASE db_name WITH (FORCE);')
-    cur.close()
-    conn.close()
-
     return dbm
-
-
-@pytest.fixture
-def fixture_for_dbm_functions(fixture_for_object_construction) -> None:
-    dbm = fixture_for_object_construction
-    dbm.creating_a_database(dbm.params, 'base')
-
-    dt = [
-        ["a", "b", "c", 1, 1, -0.0168, 51.0888, 4267.2, 'false', 189.7, 129.39, 14.63, 'null', 4282.44, "d", 'false', 0],
-        ["a", "b", "c", 1, 1, -0.0168, 51.0888, 4267.2, 'false', 189.7, 129.39, 14.63, 'null', 4282.44, "d", 'false', 0],
-        ["a", "b", "c", 1, 1, -0.0168, 51.0888, 4267.2, 'false', 189.7, 129.39, 14.63, 'null', 4282.44, "d", 'false', 0],
-        ["a", "b", "c", 1, 1, -0.0168, 51.0888, 4267.2, 'false', 189.7, 129.39, 14.63, 'null', 4282.44, "d", 'false', 0],
-        ["a", "b", "c", 1, 1, -0.0168, 51.0888, 4267.2, 'false', 189.7, 129.39, 14.63, 'null', 4282.44, "d", 'false', 0]
-    ]
-
-    dbm.creating_a_table('a', dt, 'base', dbm.params)
--- tests/test_db_manager.py
+++ tests/test_db_manager.py
@@ -1,56 +1,12 @@
-from pathlib import WindowsPath
-
-import psycopg2
-
-
 def test_object_construction(fixture_for_object_construction) -> None:
     dbm = fixture_for_object_construction
     assert dbm.db_name == 'db_name'
     assert dbm.countries == ['a']
-    assert dbm.path == WindowsPath('E:/Учеба питон/коды/5 курс/coursework/database.ini')
-    assert dbm.tables == [('tb_a',)]
+    assert dbm.path.name == 'database.ini'
+    assert dbm.tables.call_count==0
     assert dbm.params['host'] == 'localhost'
 
 
 def test_config(fixture_for_object_construction) -> None:
     dbm = fixture_for_object_construction
     assert dbm.config(dbm.path) == dbm.params
-
-
-def test_creating_a_database(fixture_for_object_construction) -> None:
-    dbm = fixture_for_object_construction
-    dbm.creating_a_database(dbm.params, 'base')
-
-    conn = psycopg2.connect(dbname='postgres', **dbm.params)
-    conn.autocommit = True
-    cur = conn.cursor()
-    cur.execute("SELECT datname FROM pg_database;")
-    tables = cur.fetchall()
-    assert ('base',) in tables
-    cur.execute('DROP DATABASE base WITH (FORCE);')
-    cur.close()
-    conn.close()
-
-def test_creating_a_table(fixture_for_object_construction)->None:
-    dbm = fixture_for_object_construction
-    dbm.creating_a_database(dbm.params, 'base')
-    dbm.creating_a_table('a', [], 'base', dbm.params)
-
-    conn = psycopg2.connect(dbname='base', **dbm.params)
-    cur = conn.cursor()
-    cur.execute("SELECT table_name FROM information_schema.tables "
-                "WHERE table_type = 'BASE TABLE' AND table_schema = 'public';")
-    tables = cur.fetchall()
-    assert tables==[('tb_a',)]
-    cur.close()
-    conn.close()
-
-    conn = psycopg2.connect(dbname='postgres', **dbm.params)
-    conn.autocommit = True
-    cur = conn.cursor()
-    cur.execute('DROP DATABASE base WITH (FORCE);')
-    cur.close()
-    conn.close()
-
-def test_get_countries_and_aeroplanes_count(fixture_for_dbm_functions)->None:
-    assert 1==1
